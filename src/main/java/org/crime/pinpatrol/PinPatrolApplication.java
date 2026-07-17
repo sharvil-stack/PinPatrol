2d4e727d15cc698c0e4e2ed20ff6c0d9ba940ea8
Sharvil Bhangre <sharvil.bhangre@gmail.com>
2026-07-17 21:29:40 +0530

    First Commit

---
+++ src/main/java/org/crime/pinpatrol/PinPatrolApplication.java
@@ -0,0 +1,13 @@
+package org.crime.pinpatrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinPatrolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinPatrolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/crime/pinpatrol/PinPatrolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crime.pinpatrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinPatrolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
