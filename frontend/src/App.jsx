031db8ad7b34a2f70b3c8fe3fdfba707d46ac5ce
Sharvil Bhangre <sharvil.bhangre@gmail.com>
2026-07-22 17:54:40 +0530

    Frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+ <h1 className="text-5xl font-bold text-orange-500">
+        PatrolPin
+      </h1>
+    </>
+  )
+}
+
+export default App
